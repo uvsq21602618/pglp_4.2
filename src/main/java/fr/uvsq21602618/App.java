5dd53db231f30be00d770c1c848506d29dd51a0d
nathalie bernard <nathalie.bernard@ens.uvsq.fr>
2020-03-26 22:24:21 +0100

    maj pom.xml

---
+++ src/main/java/fr/uvsq21602618/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq21602618;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
